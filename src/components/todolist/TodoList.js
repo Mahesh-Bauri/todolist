4e7a12857c90ed9fad808cf195486d9ac900359d
Mahesh Bauri <www.maheshbauri@gmail.com>
2023-06-24 10:39:34 +0530

    added loading and empty status

--- src/components/todolist/TodoList.js
+++ src/components/todolist/TodoList.js
@@ -5,52 +5,62 @@ import Todo from "../todo/Todo";
 import { MdAdd } from "react-icons/md";
 
 const TodoList = () => {
-  // get state and setter function from Custom Hook
-  const { todos, handleAdd, setTodoText, todoText } = useTodos();
+	// get state and setter function from Custom Hook
+	const { todos, isLoading, handleAdd, setTodoText, todoText } = useTodos();
 
-  const handleChange = (e) => {
-    setTodoText(e.target.value);
-  };
+	const handleChange = (e) => {
+		setTodoText(e.target.value);
+	};
 
-  // clear function to clear the input when user submit
-  const clear = () => {
-    setTodoText("");
-  };
+	// clear function to clear the input when user submit
+	const clear = () => {
+		setTodoText("");
+	};
 
-  return (
-    <div className="todolist-container">
-      <h1>TODO</h1>
-      <form
-        onSubmit={(e) => {
-          handleAdd(e, todoText);
-          clear();
-        }}
-        className="todolist-input"
-      >
-        <input
-          type="text"
-          placeholder="Add todo"
-          value={todoText}
-          onChange={handleChange}
-        />
-        <button>
-          <MdAdd />
-        </button>
-      </form>
-      <div className="listItem">
-        <ul>
-          {todos &&
-            todos.map((todo, i) => {
-              return (
-                <li key={i}>
-                  <Todo {...todo} />
-                </li>
-              );
-            })}
-        </ul>
-      </div>
-    </div>
-  );
+	const Loading = () => {
+		return <p className="loading">Loading...</p>;
+	};
+
+	return (
+		<div className="todolist-container">
+			<h1>TODO</h1>
+			<form
+				onSubmit={(e) => {
+					handleAdd(e, todoText);
+					clear();
+				}}
+				className="todolist-input"
+			>
+				<input
+					type="text"
+					placeholder="Add todo"
+					value={todoText}
+					onChange={handleChange}
+				/>
+				<button>
+					<MdAdd />
+				</button>
+			</form>
+			{isLoading ? (
+				<Loading />
+			) : todos.length === 0 ? (
+				<p className="empty">Empty TodoList</p>
+			) : (
+				<div className="listItem">
+					<ul>
+						{todos &&
+							todos.map((todo, i) => {
+								return (
+									<li key={i}>
+										<Todo {...todo} />
+									</li>
+								);
+							})}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default TodoList;
--- src/contexts/todoContext.js
+++ src/contexts/todoContext.js
@@ -6,193 +6,197 @@ const todoContext = createContext();
 
 // Custom Hooks
 export const useTodos = () => {
-  const value = useContext(todoContext);
-  return value;
+	const value = useContext(todoContext);
+	return value;
 };
 
 // Custom Context Provider Component
 const TodoProvider = ({ children }) => {
-  const [todoText, setTodoText] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [isEdit, setIsEdit] = useState(false);
-  const [id, setId] = useState(null);
+	const [todoText, setTodoText] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [isEdit, setIsEdit] = useState(false);
+	const [id, setId] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const addTodo = async (todo) => {
-    try {
-      var res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        body: JSON.stringify({
-          userId: 1,
-          title: todo,
-          completed: false,
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      });
-    } catch (e) {
-      console.log(e.message);
-    }
-    return await res.json();
-  };
+	const addTodo = async (todo) => {
+		try {
+			var res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+				method: "POST",
+				body: JSON.stringify({
+					userId: 1,
+					title: todo,
+					completed: false,
+				}),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+			});
+		} catch (e) {
+			console.log(e.message);
+		}
+		return await res.json();
+	};
 
-  // addind the todo in todos state
-  const handleAdd = async (e, todo) => {
-    e.preventDefault();
+	// addind the todo in todos state
+	const handleAdd = async (e, todo) => {
+		e.preventDefault();
 
-    if (!todo) {
-      toast.warn("Please add something!", {
-        autoClose: 1000,
-      });
-      return;
-    }
+		if (!todo) {
+			toast.warn("Please add something!", {
+				autoClose: 1000,
+			});
+			return;
+		}
 
-    if (isEdit) {
-      handleUpdate(todo);
-      return;
-    }
+		if (isEdit) {
+			handleUpdate(todo);
+			return;
+		}
 
-    toast.success("Todo is adding!", { autoClose: 1000 });
+		toast.success("Todo is adding!", { autoClose: 1000 });
 
-    // dummy fetch call using async function
-    const data = await addTodo(todo);
+		// dummy fetch call using async function
+		const data = await addTodo(todo);
 
-    // set Todos in todos state
-    setTodos((prev) => {
-      return [{ ...data, id: new Date().getTime() }, ...prev];
-    });
+		// set Todos in todos state
+		setTodos((prev) => {
+			return [{ ...data, id: new Date().getTime() }, ...prev];
+		});
 
-    // show toast for successfully added
-    toast.success("Added Successfully!", { autoClose: 2000 });
-  };
+		// show toast for successfully added
+		toast.success("Added Successfully!", { autoClose: 2000 });
+	};
 
-  // toggle the todo item
-  const handleCompleted = (id) => {
-    const foundTodo = todos.find((todo) => todo.id === id);
+	// toggle the todo item
+	const handleCompleted = (id) => {
+		const foundTodo = todos.find((todo) => todo.id === id);
 
-    setTodos((prev) => {
-      return prev.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      });
-    });
+		setTodos((prev) => {
+			return prev.map((todo) => {
+				if (todo.id === id) {
+					return { ...todo, completed: !todo.completed };
+				}
+				return todo;
+			});
+		});
 
-    if (!foundTodo.completed) {
-      toast.success("Task Completed!", {
-        position: toast.POSITION.BOTTOM_LEFT,
-        autoClose: 1500,
-      });
-    } else {
-      toast.success("Task Incompleted!", {
-        position: toast.POSITION.BOTTOM_LEFT,
-        autoClose: 1000,
-      });
-    }
-  };
+		if (!foundTodo.completed) {
+			toast.success("Task Completed!", {
+				position: toast.POSITION.BOTTOM_LEFT,
+				autoClose: 1500,
+			});
+		} else {
+			toast.success("Task Incompleted!", {
+				position: toast.POSITION.BOTTOM_LEFT,
+				autoClose: 1000,
+			});
+		}
+	};
 
-  const updateTodo = async (todo) => {
-    try {
-      var res = await fetch(`https://jsonplaceholder.typicode.com/todos/1`, {
-        method: "PUT",
-        body: JSON.stringify({
-          id: id,
-          title: todo.title,
-          userId: 1,
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      });
-    } catch (e) {
-      console.log(e.message);
-    }
-    return await res.json();
-  };
+	const updateTodo = async (todo) => {
+		try {
+			var res = await fetch(`https://jsonplaceholder.typicode.com/todos/1`, {
+				method: "PUT",
+				body: JSON.stringify({
+					id: id,
+					title: todo.title,
+					userId: 1,
+				}),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+			});
+		} catch (e) {
+			console.log(e.message);
+		}
+		return await res.json();
+	};
 
-  // helper function for update the todo
-  const setUpdateTodo = (todo) => {
-    window.scroll({
-      top: 0,
-      behavior: "smooth",
-    });
+	// helper function for update the todo
+	const setUpdateTodo = (todo) => {
+		window.scroll({
+			top: 0,
+			behavior: "smooth",
+		});
 
-    setTodoText(todo.title);
-    setId(todo.id);
-    setIsEdit(true);
-  };
+		setTodoText(todo.title);
+		setId(todo.id);
+		setIsEdit(true);
+	};
 
-  // update the todos state
-  const handleUpdate = async (newTodo) => {
-    toast.success("Todo is updating!", { autoClose: 1000 });
-    await updateTodo(newTodo);
-    setId(null);
+	// update the todos state
+	const handleUpdate = async (newTodo) => {
+		toast.success("Todo is updating!", { autoClose: 1000 });
+		await updateTodo(newTodo);
+		setId(null);
 
-    // update todos using id's
-    setTodos((prev) => {
-      return prev.map((todo) => {
-        if (todo.id === id) return { ...todo, title: newTodo };
-        return todo;
-      });
-    });
+		// update todos using id's
+		setTodos((prev) => {
+			return prev.map((todo) => {
+				if (todo.id === id) return { ...todo, title: newTodo };
+				return todo;
+			});
+		});
 
-    toast.success("Updated Successfully!", { autoClose: 2000 });
-    setIsEdit(false);
-  };
+		toast.success("Updated Successfully!", { autoClose: 2000 });
+		setIsEdit(false);
+	};
 
-  // helper function
-  const deleteTodo = async (id) => {
-    try {
-      await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-        method: "DELETE",
-      });
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
+	// helper function
+	const deleteTodo = async (id) => {
+		try {
+			await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+				method: "DELETE",
+			});
+		} catch (e) {
+			console.log(e.message);
+		}
+	};
 
-  // delete function for deleting the todo
-  const handleDelete = async (idx) => {
-    toast.success("Todo is deleting!", { autoClose: 1000 });
-    if (idx === id) {
-      setTodoText("");
-    }
-    setIsEdit(false);
-    await deleteTodo(idx);
-    toast.success("Deleted Successfully!", { autoClose: 2000 });
-    setTodos((prev) => {
-      return prev.filter((todo) => idx !== todo.id);
-    });
-  };
+	// delete function for deleting the todo
+	const handleDelete = async (idx) => {
+		toast.success("Todo is deleting!", { autoClose: 1000 });
+		if (idx === id) {
+			setTodoText("");
+		}
+		setIsEdit(false);
+		await deleteTodo(idx);
+		toast.success("Deleted Successfully!", { autoClose: 2000 });
+		setTodos((prev) => {
+			return prev.filter((todo) => idx !== todo.id);
+		});
+	};
 
-  // async function to fetch todos
-  const getTodos = async (url) => {
-    const res = await fetch(url);
-    const data = await res.json();
-    setTodos(data);
-  };
+	// async function to fetch todos
+	const getTodos = async (url) => {
+		const res = await fetch(url);
+		const data = await res.json();
+		setTodos(data.slice(0, 6));
+		setIsLoading(false);
+	};
 
-  // to get all list of todos and set todos state
-  useEffect(() => {
-    getTodos("https://jsonplaceholder.typicode.com/todos");
-  }, []);
+	// to get all list of todos and set todos state
+	useEffect(() => {
+		setIsLoading(true);
+		getTodos("https://jsonplaceholder.typicode.com/todos");
+	}, []);
 
-  return (
-    <todoContext.Provider
-      value={{
-        todos,
-        handleAdd,
-        todoText,
-        setTodoText,
-        handleCompleted,
-        handleDelete,
-        setUpdateTodo,
-      }}
-    >
-      {children}
-    </todoContext.Provider>
-  );
+	return (
+		<todoContext.Provider
+			value={{
+				todos,
+				handleAdd,
+				todoText,
+				setTodoText,
+				handleCompleted,
+				handleDelete,
+				setUpdateTodo,
+				isLoading,
+			}}
+		>
+			{children}
+		</todoContext.Provider>
+	);
 };
 
 export default TodoProvider;
